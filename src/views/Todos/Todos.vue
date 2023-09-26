98c8f626fb5c8687d95e3bafce90f9a305202bc9
ricardo <ricardo@in8.com.br>
2023-09-26 18:50:48 -0300

    Change todo view

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,13 +1,41 @@
 <template>
   <h1>Lista de tarefas</h1>
+
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.name }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
+  setup() {
+    const todos = [
+      {
+        id: 1,
+        name: 'Tarefa 01',
+        completed: true
+      },
+      {
+        id: 2,
+        name: 'Tarefa 02',
+        completed: true
+      },
+      {
+        id: 3,
+        name: 'Tarefa 03',
+        completed: true
+      }
+    ]
 
+    return {
+      todos
+    }
+  }
 }
 </script>
 
-<style>
+<style scope>
 
 </style>
