67e23871fcb2e5c78012eadb197f5eaac6a66145
ricardo <ricardo@in8.com.br>
2023-10-02 19:24:12 -0300

    connect api, make loading

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>Lista de tarefas</h1>
 
+  <div v-show="loading">Carregando as tarefas</div>
+
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       {{ todo.name }}
@@ -9,27 +11,25 @@
 </template>
 
 <script>
+import { onMounted, ref } from 'vue'
+import TodoService from '@/services/todos.service'
+
 export default {
   setup() {
-    const todos = [
-      {
-        id: 1,
-        name: 'Tarefa 01',
-        completed: true
-      },
-      {
-        id: 2,
-        name: 'Tarefa 02',
-        completed: true
-      },
-      {
-        id: 3,
-        name: 'Tarefa 03',
-        completed: true
-      }
-    ]
+    const todos = ref([])
+
+    const loading = ref(false)
+
+    onMounted(() => {
+      loading.value = true
+      TodoService.getAll()
+               .then(response => todos.value = response.data.data)
+               .catch(error => console.log(error))
+               .finally(() => loading.value = false)
+    })
 
     return {
+      loading,
       todos
     }
   }
